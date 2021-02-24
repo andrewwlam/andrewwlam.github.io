5976fae18ee43d9be0eaa2ae32a5835b660cf7f3
andrewwlam <andrew.l.0810@gmail.com>
2021-02-24 15:01:41 -0500

    commit on main

--- andrewwlam/js/main.js
+++ andrewwlam/js/main.js
@@ -101,4 +101,17 @@
 		});
 	}
 
+	/*--/ Testimonials owl /--*/
+	$('#testimonial-mf').owlCarousel({
+		margin: 20,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			}
+		}
+	});
+
 })(jQuery);
